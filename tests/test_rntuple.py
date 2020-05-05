58d21b5d03c3ee04d28ce46d631ec0f3d2f85587
Oksana Shadura <oksana.shadura@cern.ch>
2020-05-05 15:23:47 +0200

    Adjust references for new rntuple files

--- tests/test_rntuple.py
+++ tests/test_rntuple.py
@@ -31,12 +31,12 @@ class Test(object):
         rfile = f["F"]
         assert rfile._fVersion == 0
         assert rfile._fSize == 48
-        assert rfile._fSeekHeader == 854
-        assert rfile._fNBytesHeader == 537
-        assert rfile._fLenHeader == 2495
-        assert rfile._fSeekFooter == 72369
-        assert rfile._fNBytesFooter == 285
-        assert rfile._fLenFooter == 804
+        assert rfile._fSeekHeader == 848
+        assert rfile._fNBytesHeader == 365
+        assert rfile._fLenHeader == 1759
+        assert rfile._fSeekFooter == 3202579
+        assert rfile._fNBytesFooter == 651
+        assert rfile._fLenFooter == 2064
         assert rfile._fReserved == 0
         
     def test_open_file_lhcb_opendata(self):
@@ -46,12 +46,12 @@ class Test(object):
         rfile = f["DecayTree"]
         assert rfile._fVersion == 0
         assert rfile._fSize == 48
-        assert rfile._fSeekHeader == 854
-        assert rfile._fNBytesHeader == 537
-        assert rfile._fLenHeader == 2495
-        assert rfile._fSeekFooter == 72369
-        assert rfile._fNBytesFooter == 285
-        assert rfile._fLenFooter == 804
+        assert rfile._fSeekHeader == 876
+        assert rfile._fNBytesHeader == 846
+        assert rfile._fLenHeader == 5206
+        assert rfile._fSeekFooter == 145085
+        assert rfile._fNBytesFooter == 461
+        assert rfile._fLenFooter == 1480
         assert rfile._fReserved == 0
 
 
@@ -62,10 +62,10 @@ class Test(object):
         rfile = f["Events"]
         assert rfile._fVersion == 0
         assert rfile._fSize == 48
-        assert rfile._fSeekHeader == 854
-        assert rfile._fNBytesHeader == 537
-        assert rfile._fLenHeader == 2495
-        assert rfile._fSeekFooter == 72369
-        assert rfile._fNBytesFooter == 285
+        assert rfile._fSeekHeader == 858
+        assert rfile._fNBytesHeader == 612
+        assert rfile._fLenHeader == 2818
+        assert rfile._fSeekFooter == 39880
+        assert rfile._fNBytesFooter == 257
         assert rfile._fLenFooter == 804
         assert rfile._fReserved == 0
